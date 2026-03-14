df06fb42cd73e9c3962f8ac44fe27d375935f04c
Akhila <akhilak0103@gmail.com>
2026-03-13 22:01:56 -0500

    Initial Commit

---
+++ Hair AI/Hair_AIApp.swift
@@ -0,0 +1,32 @@
+//
+//  Hair_AIApp.swift
+//  Hair AI
+//
+//  Created by Akhila Katepalli on 3/13/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Hair_AIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
